64080349408b3ffc6c1ff22472737d7fb4095d64
adhu007 <adarshpdas98@gmail.com>
2025-05-30 11:32:36 +0530

    Configured Github Pages

--- src/main.jsx
+++ src/main.jsx
@@ -2,19 +2,19 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, HashRouter } from 'react-router-dom'
 import AppContextProvider from './context/AppContext.jsx'
 import ShopContextProvider from './context/ShopContext.jsx'
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <AppContextProvider>
         <ShopContextProvider>
             <App />
         </ShopContextProvider>
       </AppContextProvider> 
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>,
 )
